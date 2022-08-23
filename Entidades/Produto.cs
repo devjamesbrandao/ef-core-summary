64763ea9755256539a345ef7f15738fd3c61b4a3
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-08-22 21:45:40 -0300

    feat: add entidade de produto

---
+++ Entidades/Produto.cs
@@ -0,0 +1,10 @@
+namespace Curso.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
